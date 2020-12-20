839a165f74943abd2e33e49f9cf8d958427857d4
Lexus <rounin_lopez@yahoo.com>
2020-12-19 22:31:10 -0600

    Database(functions(add customer),models(customerModel))

--- WebForm/WebApplication1/WebApplication1/Database/Database.cs
+++ WebForm/WebApplication1/WebApplication1/Database/Database.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.OleDb;
+using WebApplication1.Models;
 
 namespace WebApplication1.Database
 {
@@ -21,11 +22,25 @@ namespace WebApplication1.Database
         #region Constructors
         public Database()
         {
+            OleDbConnectionStringBuilder builder = new OleDbConnectionStringBuilder();
 
+            builder.Provider = "Microsoft.Jet.OLEDB.4.0;";
+            builder.DataSource = dbRoot + "CustomersDb.mdb";
+            builder.PersistSecurityInfo = true;
+
+            con = new OleDbConnection(builder.ConnectionString);
         }
         #endregion
         #region Functions
+        /*todo C.R.U.D */
 
+        public Customers CreateEntry(Customers newCustomer)
+        {
+            Customers returnCustomer = default;
+
+
+            return returnCustomer;
+        }
         #endregion
     }
 }
---
+++ WebForm/WebApplication1/WebApplication1/Models/Customers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Customers
+    {
+        #region Properties
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        #endregion
+    }
+}
