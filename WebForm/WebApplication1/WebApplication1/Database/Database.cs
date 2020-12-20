34115fc8858d6bc0d103e64cc4ef1f05b6e91f1d
Lexus <rounin_lopez@yahoo.com>
2020-12-19 22:15:50 -0600

    setting shit up

---
+++ WebForm/WebApplication1/WebApplication1/Database/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.OleDb;
+
+namespace WebApplication1.Database
+{
+    public class Database
+    {
+        #region Variables
+        private readonly string dbRoot = HttpContext.Current.Server.MapPath("~/App_Data/");
+        private OleDbConnection con = default;
+        #endregion
+        #region Properties
+
+        #endregion
+        #region NestedClasses
+
+        #endregion
+        #region Constructors
+        public Database()
+        {
+
+        }
+        #endregion
+        #region Functions
+
+        #endregion
+    }
+}
