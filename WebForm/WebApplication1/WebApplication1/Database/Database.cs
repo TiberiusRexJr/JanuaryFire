a57d532052a304dbfc8abdb5ecdc488f27ee358f
Lexus <rounin_lopez@yahoo.com>
2020-12-20 04:09:54 -0600

    Database(Function(CreateEntity))

--- WebForm/WebApplication1/WebApplication1/Database/Database.cs
+++ WebForm/WebApplication1/WebApplication1/Database/Database.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.OleDb;
+using System.Data;
+using System.Data.SqlClient;
 using WebApplication1.Models;
+using WebApplication1.Utilities;
+
 
 namespace WebApplication1.Database
 {
@@ -12,6 +16,7 @@ namespace WebApplication1.Database
         #region Variables
         private readonly string dbRoot = HttpContext.Current.Server.MapPath("~/App_Data/");
         private OleDbConnection con = default;
+        public ErrorHandling er = new ErrorHandling();
         #endregion
         #region Properties
 
@@ -34,13 +39,75 @@ namespace WebApplication1.Database
         #region Functions
         /*todo C.R.U.D */
 
-        public Customers CreateEntry(Customers newCustomer)
+        #region CRUD
+
+
+        #region Create
+        public bool CreateEntry(Customers newCustomer)
         {
-            Customers returnCustomer = default;
+            bool status = false;
+            int rowsAffect = default;
+            string query = "INSERT into Customers(@Name,@Address,@City,@State,@Zip) VALUES(?,?,?,?,?)";
 
+            OleDbCommand command = new OleDbCommand(query, con);
+            command.Parameters.AddWithValue("@Name", newCustomer.Name);
+            command.Parameters.AddWithValue("@Address", newCustomer.Address);
+            command.Parameters.AddWithValue("@City", newCustomer.City);
+            command.Parameters.AddWithValue("@State", newCustomer.State);
+            command.Parameters.AddWithValue("@Zip", newCustomer.Zip);
+            
 
-            return returnCustomer;
+            try
+            {
+                con.Open();
+               rowsAffect= command.ExecuteNonQuery();
+            }
+            catch (OleDbException e)
+            {
+                er.ErrorheadOleDbException(e);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            if(rowsAffect>0)
+            {
+                status = true;
+            }
+
+            return status;
         }
+        #endregion
+
+        #region Retrieve
+        public List<Customers> GetAllCustomers()
+        {
+            List<Customers> customers = new List<Customers>();
+
+            return customers;
+        }
+        #endregion
+
+        #region Update
+
+        #endregion
+
+        #region Delete
+
+        #endregion
+        #endregion
+
+        #region TableInfo
+        List<string> GetColumnHeaders()
+        {
+            List<string> columnHeaders = new List<string>();
+
+
+            return columnHeaders;
+        }
+        #endregion
+
         #endregion
     }
 }
