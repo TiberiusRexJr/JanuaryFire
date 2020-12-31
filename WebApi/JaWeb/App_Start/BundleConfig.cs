d8185565a25f3a901344073d16f7737d9e2c01cd
Lexus <rounin_lopez@yahoo.com>
2020-12-30 18:44:13 -0600

    WebApi all dependencies add, basic front end design setup

---
+++ WebApi/JaWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.UI;
+
+namespace JaWeb
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit https://go.microsoft.com/fwlink/?LinkID=303951
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/WebFormsJs").Include(
+                            "~/Scripts/WebForms/WebForms.js",
+                            "~/Scripts/WebForms/WebUIValidation.js",
+                            "~/Scripts/WebForms/MenuStandards.js",
+                            "~/Scripts/WebForms/Focus.js",
+                            "~/Scripts/WebForms/GridView.js",
+                            "~/Scripts/WebForms/DetailsView.js",
+                            "~/Scripts/WebForms/TreeView.js",
+                            "~/Scripts/WebForms/WebParts.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/JqueryBootstrapSeralizetoJsonValidate").Include("~/Scripts/jquery-3.5.1.js", "~/Scripts/bootstrap.min.js", "~/Scripts/jquery.serializeToJSON.js","~/Scripts/popper.min.js", "~/Scripts/jquery.dataTables.min.js", "~/Scripts/jquery.validate.js"));
+            bundles.Add(new StyleBundle("~/bundles/SiteBootstrapDataTables").Include("~/Content/Site.css","~/Content/bootstrap.min.css", "~/Content/jquery.dataTables.min.css"));
+
+            // Order is very important for these files to work, they have explicit dependencies
+            bundles.Add(new ScriptBundle("~/bundles/MsAjaxJs").Include(
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjax.js",
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js",
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/FontAwesome").Include(
+                "~/Content/FontAwesome4/css/font-awesome.min.css"
+                ));
+
+            // Use the Development version of Modernizr to develop with and learn from. Then, when you’re
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                            "~/Scripts/modernizr-*"));
+        }
+    }
+}
---
+++ WebApi/JaWeb/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="JaWeb._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+<div class="card">
+  <h3 class="card-header"><i class="fa fa-cloud fa-2x mx-1 my-1"></i>Invoice Cloud</h3>
+  <div class="card-body">
+    <h5 class="card-title">Customers</h5>
+    
+      <p>
+  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+   <i class="fa fa-user-plus fa-2x"></i> Add Customer
+  </button>
+</p>
+
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+  </div>
+</div>
+
+
+  </div>
+</div>
+
+
+</asp:Content>
